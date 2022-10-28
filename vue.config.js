34419d8273dd89341ea3548670a2e98383462f18
unknown <asd8296310@gmail.com>
2022-10-28 09:38:40 +0800

    upload

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false //關閉eslint校驗
-})
+  lintOnSave: false, //關閉eslint校驗
 
-module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
   ? '/API2/'
   : '/'
-}
+})
